2c16b72a49b044b9ba1ebeb492d8558cc3ae9527
fleekwhat <fleekwhat@gmail.com>
2025-11-06 15:31:40 +0900

    feat: 첫번째 기능(GET/index.html)

--- shop/src/main/java/com/apple/shop/ShopApplication.java
+++ shop/src/main/java/com/apple/shop/ShopApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.apple.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShopApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ShopApplication.class, args);
+    }
 }
